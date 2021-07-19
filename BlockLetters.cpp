941ede9a25b27b509b8b7e62f826d720b8dd5796
Dair66 <alasdairsw.anderson@gmail.com>
2021-07-19 09:04:36 +0100

    Finished BlockLetters Project
    The aim of this project was to get to grips with printing to the terminal. To do this i had to create multiple cout actions so that my initials are printed to the console.

---
+++ BlockLetters.cpp
@@ -0,0 +1,12 @@
+//fun fact about my self is that i am trying to learn C++ to get into the gaming industry
+#include <iostream>
+
+int main(){
+  std::cout << "   A         A  \n";
+  std::cout << " A   A     A   A\n";
+  std::cout << "A A A A   A A A A\n";
+  std::cout << "A     A   A     A\n";
+  std::cout << "A     A , A     A\n";
+  
+  return 0;
+}
