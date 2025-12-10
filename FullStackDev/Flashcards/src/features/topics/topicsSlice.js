f92828cbb53ddbbf6fa998065aeedcb12b8dea70
Alasdair Anderson <alasdairsw.anderson@gmail.com>
2025-12-10 05:45:38 +0000

    Add files via upload

---
+++ FullStackDev/Flashcards/src/features/topics/topicsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, createAction } from "@reduxjs/toolkit";
+import { addQuiz } from "../quizzes/quizzesSlice";
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: {},
+    },
+    reducers: {
+        addTopic(state, action) {
+            const { id, name, icon } = action.payload;
+            state.topics[id] = {
+                id: id,
+                name: name,
+                icon: icon,
+                quizIds: []
+            }
+        }
+    },
+    selectors: {
+        selectTopics: (state) => state.topics
+    },
+    extraReducers: (builder) => {
+        builder.addCase(addQuiz, (state, action) => {
+                const { id, topicId } = action.payload;
+                state.topics[topicId].quizIds.push(id)
+            }
+        )
+    }
+});
+
+export const { addTopic } = topicsSlice.actions;
+export const selectTopics = (state) => state.topics.topics;
+export default topicsSlice.reducer;
